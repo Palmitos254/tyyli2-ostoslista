0e74c55ff3efa8777a8af77de583b3aecc75259e
Palmito254 <bgr921@myy.haaga-helia.fi>
2022-10-25 23:09:05 +0300

    f

--- App.js
+++ App.js
@@ -1,11 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, TextInput } from 'react-native';
+import { Header, Icon, Input, Button, ListItem } from 'react-native-elements';
+import { useEffect, useState } from 'react';
+import * as SQLite from'expo-sqlite';
 
 export default function App() {
+
+  const [product, setProduct] = useState('');
+  const [amount, setAmount] = useState('');
+  const [shoppinglist, setShoppinglist] = useState([]);
+  const db = SQLite.openDatabase('coursedb.db');
+
+  useEffect(() => {
+    db.transaction(tx => {
+      tx.executeSql('create table if not exists shoppinglist (id integer primary key not null, product text, amount text);');
+    }, null, updateList);
+  }, []);
+
+  const saveItem = () => {
+    db.transaction(tx => {
+      tx.executeSql('insert into shoppinglist (product, amount) values (?, ?);',  [product, amount]);
+    }, null, updateList)
+  }
+
+  const updateList = () => {
+    db.transaction(tx => {
+      tx.executeSql('select * from shoppinglist;', [], (_, { rows }) =>
+      setShoppinglist(rows._array)
+      );
+    }, null, null);
+  }
+
+  const deleteItem = (id) => {
+    db.transaction(tx => {
+      tx.executeSql(`delete from shoppinglist where id = ?;`, [id]);
+    }, null, updateList)    
+  }
+
+  renderItem = ({ item }) => (
+    <ListItem bottomDivider>
+      <ListItem.Content>
+        <ListItem.Title>{item.product}</ListItem.Title>
+        <ListItem.Subtitle>{item.amount}</ListItem.Subtitle>
+      </ListItem.Content>
+    </ListItem>
+    );
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header
+        centerComponent={{ text: 'SHOPPING LIST', style: { color: '#fff' } }}
+      />
+      <Input
+        placeholder='Product' label='PRODUCT'
+        onChangeText={product => setProduct(product)} value={product}
+      />
+      <Input
+        placeholder='Amount' label='AMOUNT'
+        onChangeText={amount => setAmount(amount)} value={amount}
+      />
+      <Button raised icon={{name: 'save'}} onPress={saveItem} title="SAVE" />
+      <FlatList
+        style={{marginLeft: "5%"}}
+        keyExtractor={item => item.id.toString()}
+        renderItem={({item}) =>
+          <View style={styles.listcontainer}>
+            <Text>{item.product}, {item.amount} </Text>
+            <Icon type="material" color="red" name="delete" onPress={() => deleteItem(item.id)}/>
+          </View>}
+        data={shoppinglist}
+      />
     </View>
   );
 }
@@ -17,4 +80,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  listcontainer: {
+    flexDirection: 'row',
+    backgroundColor: '#fff',
+    alignItems: 'center'
+   },
 });
